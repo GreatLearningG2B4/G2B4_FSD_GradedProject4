c9feb1bd818928f60e171d34e46fc1886d8c00c8
iamsubendu <66836529+iamsubendu@users.noreply.github.com>
2024-06-03 21:40:06 +0530

    added final changes

--- src/main/java/com/gl/EmpManagment/service/impl/UserServiceImpl.java
+++ src/main/java/com/gl/EmpManagment/service/impl/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.gl.EmpManagment.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gl.EmpManagment.repository.RoleRepository;
 import com.gl.EmpManagment.repository.UserRepository;
+import com.gl.EmpManagment.security.entity.Role;
 import com.gl.EmpManagment.security.entity.User;
 import com.gl.EmpManagment.service.UserService;
 
@@ -13,10 +15,18 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserRepository userRepo;
 	
+	@Autowired
+	private RoleRepository roleRepo;
+	
 	@Override
 	public User save(User user) {
 	
 		return userRepo.save(user);
+	}
+
+	@Override
+	public Role save(Role role) {
+		return roleRepo.save(role);
 	}
 
 }
